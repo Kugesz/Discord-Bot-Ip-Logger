285c6a1a3c1647c96b70a3eb9bb7579a046d7ff9
Kugesz <kovacsgergo.algyo@gmail.com>
2024-12-23 00:58:07 +0100

    changed to docker

--- get_running_containers.py
+++ get_running_containers.py
@@ -1,14 +1,7 @@
-import subprocess
+import docker
 
 def get_running_containers():
-    try:
-        # Run the 'docker ps' command and capture the output
-        result = subprocess.run(['sudo', 'docker', 'ps'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
-        containers = result.stdout.strip().split('\n')
-        return containers
-    except subprocess.CalledProcessError as e:
-        print(f"Error running docker ps: {e.stderr}")
-        return []
-    except FileNotFoundError:
-        print("Docker is not installed or not found in PATH.")
-        return []
+    client = docker.from_env()
+
+    # List all running containers
+    containers = client.containers.list()
